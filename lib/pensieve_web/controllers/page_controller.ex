5f69cb0f93a30c2c6990f6e26cd386046b563794
Jacob Kjeldahl <jacob@kjeldahl.it>
2022-11-10 18:23:02 +0100

    Added bootstrap

--- lib/pensieve_web/controllers/page_controller.ex
+++ lib/pensieve_web/controllers/page_controller.ex
@@ -4,4 +4,12 @@ defmodule PensieveWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def about(conn, _params) do
+    render(conn, "about.html", message: "Grettings to Jake Moosey")
+  end
+
+  def info(conn, _params) do
+    render(conn, "info.html")
+  end
 end
--- lib/pensieve_web/router.ex
+++ lib/pensieve_web/router.ex
@@ -18,6 +18,8 @@ defmodule PensieveWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/about", PageController, :about
+    get "/info", PageController, :info
   end
 
   # Other scopes may use custom stacks.
