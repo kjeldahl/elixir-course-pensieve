cb89f65ced74135b2ea06c4e63b8552c1c1eaf4c
Jacob Kjeldahl <jacob@kjeldahl.it>
2023-02-25 15:25:07 +0100

    Adds edit and update actions
    Extracts the form into a separate file

--- lib/pensieve/memories/memory.ex
+++ lib/pensieve/memories/memory.ex
@@ -15,6 +15,6 @@ defmodule Pensieve.Memories.Memory do
     |> cast(attrs, [:title, :content])
     |> validate_required([:title])
     |> validate_length(:title, max: 100)
-    |> validate_format(:content, ~r/\A((?!voldemort).)*\z/i, message: "We do not speak of the eviil one")
+    |> validate_format(:content, ~r/^((?!voldemort).)*$/i, message: "We do not speak of the eviil one")
   end
 end
--- lib/pensieve_web/controllers/memory_controller.ex
+++ lib/pensieve_web/controllers/memory_controller.ex
@@ -9,6 +9,12 @@ defmodule PensieveWeb.MemoryController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  def edit(conn, %{"id" => id}) do
+    memory = Memories.get_memory!(id)
+    changeset = Memories.change_memory(memory)
+    render(conn, "edit.html", changeset: changeset, memory: memory)
+  end
+
   def create(conn, %{"memory" => memory_params}) do
     case Memories.create_memory(memory_params) do
       {:ok, memory} ->
@@ -17,6 +23,18 @@ defmodule PensieveWeb.MemoryController do
         |> redirect(to: Routes.memory_path(conn, :show, memory))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def update(conn, %{"memory" => memory_params, "id" => id}) do
+    memory = Memories.get_memory!(id)
+    case Memories.update_memory(memory, memory_params) do
+      {:ok, memory} ->
+        conn
+        |> put_flash(:info, "Update memory")
+        |> redirect(to: Routes.memory_path(conn, :show, memory))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", changeset: changeset, memory: memory)
     end
   end
 
